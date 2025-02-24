1129164caa5f8745894b63338763a03651d8ec75
CocoWantsACake <maxime27130@outlook.fr>
2025-02-24 10:05:37 +0100

    STEP 3 DONE

--- python/test_step3.py
+++ python/test_step3.py
@@ -1,3 +1,5 @@
+import time
+
 from playwright.sync_api import expect
 
 
@@ -16,28 +18,27 @@ def test_update_address(page):
     page.goto("https://e.lsi2.hr.dmerej.info/add_employee")
 
     # Create an employee
-    test = page.locator('textbox[name="Name"]')
-    test.fill("a")
-
-    page.locator('input[name="Email"]').fill("a@b.fr")
-    page.locator("#id_address_line1").fill(address_line_1)
-    page.locator("#id_address_line2").fill(address_line_2)
-    page.locator('input[name="City"]').fill("c")
-    page.locator('input[name="Zip code"]').fill("27130")
-    page.locator('input[name="Hiring date"]').fill("2002-08-30")
-    page.locator('input[name="Job title"]').fill("d")
-    page.locator('button[name="Add"]').click()
+    page.locator('input[name="name"]').fill("a")
+    page.locator('input[name="email"]').fill("a@b.fr")
+    page.locator('input[name="address_line1"]').fill(address_line_1)
+    page.locator('input[name="address_line2"]').fill(address_line_2)
+    page.locator('input[name="city"]').fill("c")
+    page.locator('input[name="zip_code"]').fill("27130")
+    page.locator('input[name="hiring_date"]').fill("2002-08-30")
+    page.locator('input[name="job_title"]').fill("d")
+    page.get_by_role("button", name="Add").click()
 
     # Edit an employee
-    page.locator('button[name="Edit"]').click()
-    expect(page.locator("#id_address_line2")).to_contain_text(address_line_1)
-    expect(page.locator("#id_address_line2")).to_contain_text(address_line_2)
-    page.locator("#id_address_line1").fill(address_line_1_update)
-    page.locator("#id_address_line2").fill(address_line_2_update)
-    page.locator('button[name="Update"]').click()
+    page.get_by_role("link", name="Edit").click()
+    page.get_by_role("link", name="Update address").click()
+    expect(page.locator('input[name="address_line1"]')).to_have_value(address_line_1)
+    expect(page.locator('input[name="address_line2"]')).to_have_value(address_line_2)
+    page.locator('input[name="address_line1"]').fill(address_line_1_update)
+    page.locator('input[name="address_line2"]').fill(address_line_2_update)
+    page.get_by_role("button", name="Update").click()
 
-    # Check that the values are saved CORRECTLY UWU
-    page.locator('button[name="Edit"]').click()
-    expect(page.locator("#id_address_line2")).to_contain_text(address_line_1_update)
-    expect(page.locator("#id_address_line2")).to_contain_text(address_line_2_update)
+    # Check that the values are saved CORRECTLY
+    page.get_by_role("link", name="Update address").click()
+    expect(page.locator('input[name="address_line1"]')).to_have_value(address_line_1_update)
+    expect(page.locator('input[name="address_line2"]')).to_have_value(address_line_2_update)
 
